c039586c7c94bbdcd43082fe4f74ce756cad2f2c
JeongWonLim <cdnljw@gmail.com>
2022-03-16 15:16:17 +0900

    view engine 연결- set함수 render 함수 - 페이지 파싱

--- login-lecture/app.js
+++ login-lecture/app.js
@@ -1,33 +1,16 @@
 const express = require("express")
 const app = express();
-const html = `
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="UTF-8">
-  <title>practice</title>
-</head>
-<body>
-  <h1>한글 되네?</h1>
-  <h2>신기하구나.. 너란 녀석....</h2>
-  <form action='/'>
-    <b>아이디</b>
-    <input type="text" name="id" placeholder="ID" method="post" />
-    <b>비밀번호</b>
-    <input type="password" name="password" placeholder="PW" method="post" />
-    <button>확인</button>
-  </form>
-</body>
-</html>
-`;
+
+//연결
+app.set("views", "./views");
+app.set("view engine", "ejs");
 
 app.get("/", (req, res)=> {
-    res.send(html);
-    res.send("여기는 루트입니다")// send는 한번밖에 보내지 못하기때문에 2번째 send는 무시
+    res.render("./home/index.ejs")
 });
 
 app.get("/login", (req, res)=>{
-    res.send("여기는 로그인입니다");
+    res.render("./home/login.ejs");
 }); 
 //send는 자동으로 Content-type 정의해줌
 
