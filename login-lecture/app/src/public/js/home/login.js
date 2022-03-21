e51f2b996153c7ba7f1412c7786bfa67c828a83e
JeongWonLim <cdnljw@gmail.com>
2022-03-21 23:05:24 +0900

    돔을 이용하여 html 객체 저장

--- login-lecture/app/src/public/js/home/login.js
+++ login-lecture/app/src/public/js/home/login.js
@@ -1,4 +1,18 @@
 "use strict";
 
-console.log("hello");
-console.log("hi");
+//DOM -> Document Object Model
+
+const id = document.querySelector("#id"),
+    password = document.querySelector("#password"),
+    loginBtn = document.querySelector("#loginBtn");
+
+
+loginBtn.addEventListener("click", login);
+
+function login(){
+    const req ={
+        id: id.value,
+        password: password.value
+    };
+    console.log(req);
+}
